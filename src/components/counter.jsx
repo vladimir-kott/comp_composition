6baf0afb715aa7030985cdf2cac9a0c37f05345d
Vladimir Kott <vladimirkott19@gmail.com>
2021-12-08 19:14:21 +0200

    add deleted functional

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -26,7 +26,7 @@ const Counter = (props) => {
         <span className={getBadgeClasses()}>{formatCount()}</span>
         <button className='btn btn-primary btn-sm m-2' onClick={hendleIncrement}>+</button>
         <button className='btn btn-primary btn-sm m-2' onClick={hendleDecrement}>-</button>
-        <button className='btn btn-danger btn-sm m-2' onClick={props.onDelete}>Delete</button>
+        <button className='btn btn-danger btn-sm m-2' onClick={() => props.onDelete(props.id)}>Delete</button>
     </div>)
 }
 
--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -3,20 +3,29 @@ import Counter from "./counter"
 
 const CountersList = () => {
     const [counters, setCounters] = useState([
-        {id:0, value:0, name: 'Ненужная вещ'},
+        {id:0, value:0, name: 'Ненужная вещ', price: 200},
         {id:1, value:0, name: 'Ложка'},
         {id:2, value:0, name: 'Вилка'},
         {id:3, value:0, name: 'Тарелка'},
         {id:4, value:1, name: 'Набор минималиста'}
     ])
 
-    const hendlerDelete = () => {
-        console.log('del')
+    const hendlerDelete = (id) => {
+        /*console.log('del', id)*/
+        const newCounters = counters.filter(counter => counter.id !== id)
+        /*console.log(newCounters)*/
+        setCounters(newCounters)
     }
 
     return <>
         {counters.map(count=>
-        <Counter key = {count.id} value = {count.value} name = {count.name} onDelete = {hendlerDelete}>
+        <Counter 
+            key = {count.id} 
+            /*id = {count.id} 
+            value = {count.value} 
+            name = {count.name} */
+            onDelete = {hendlerDelete}
+            {...count}>
         </Counter>
         )}
     </>
